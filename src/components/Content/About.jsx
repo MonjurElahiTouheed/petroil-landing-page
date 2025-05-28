c15bd8bf127e1638ca8f3c0d2e87e3d4b0cc57d7
Monjur Elahi Touheed <monjurelahitouheed@gmail.com>
2025-05-28 14:59:05 +0600

    about section done

--- src/components/Content/About.jsx
+++ src/components/Content/About.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import Container from '../Layout/Container';
+import Flex from '../Layout/Flex';
+import logo1 from "../../assets/logo1.png"
+import logo2 from "../../assets/logo2.png"
+import logo3 from "../../assets/logo3.png"
+import logo4 from "../../assets/logo4.png"
 
 const About = () => {
     return (
         <div>
-            
+            <div className='bg-[#F0F0F0] py-[136px]'>
+                <Container>
+                    <Flex className='bg-primary justify-between'>
+                        <div>
+                            <h3 className="font-poppins font-bold text-4xl w-[385px] text-white py-[100px] pl-[74px] pr-[77px] leading-12">Learn more about our company</h3>
+                        </div>
+                        <div className='bg-[url(assets/about.png)] pt-[168px] pb-[142px] px-[282px]'>
+                            <button className='font-primary text-primary bg-white px-[30px] py-3.5 font-semibold text-base'>Learn More</button>
+                        </div>
+                    </Flex>
+                </Container>
+            </div>
+            <div className="my-[117px]">
+                <Container>
+                <Flex className='justify-between'>
+                    <img src={logo1} alt="" />
+                    <img src={logo2} alt="" />
+                    <img src={logo3} alt="" />
+                    <img src={logo4} alt="" />
+                </Flex>
+            </Container>
+            </div>
         </div>
     );
 };
--- src/components/Content/Blog.jsx
+++ src/components/Content/Blog.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Container from '../Layout/Container';
+import Flex from '../Layout/Flex';
 
 const Blog = () => {
     return (
-        <div>
-            
-        </div>
+        <section className='py-[110px] bg-[#F0F0F0]'>
+            <Container>
+                <Flex className="justify-between">
+                    <div className="pt-[75px] pb-[55px]">
+                        <h4 className='font'>lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod.</h4>
+                    </div>
+                </Flex>
+            </Container>
+        </section>
     );
 };
 
