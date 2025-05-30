a182470aa2e22806b7e94334ef6b5d63f6198e3d
Monjur Elahi Touheed <monjurelahitouheed@gmail.com>
2025-05-30 18:48:06 +0600

    blog images updated

--- src/components/Content/Blog.jsx
+++ src/components/Content/Blog.jsx
@@ -15,7 +15,7 @@ const Blog = () => {
                         <h4 className='font-primary text-2xl font-bold text-white w-[209px]'>lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod.</h4>
                         <button className='bg-white/50 text-white mt-[52px] font-primary font-semibold text-xs px-[23px] py-[11px]'>Read more</button>
                     </div>
-                    <div className="pt-[75px] pb-[55.28px] bg-[url(assets/blog1.png)] px-[45px] bg-black/60 bg-blend-multiply w-[339px]">
+                    <div className="pt-[75px] pb-[55.28px] bg-[url(assets/blog3.png)] px-[45px] bg-black/60 bg-blend-multiply w-[339px]">
                         <h4 className='font-primary text-2xl font-bold text-white w-[209px]'>lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod.</h4>
                         <button className='bg-white/50 text-white mt-[52px] font-primary font-semibold text-xs px-[23px] py-[11px]'>Read more</button>
                     </div>
