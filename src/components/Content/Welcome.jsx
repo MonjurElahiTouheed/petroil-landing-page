1b5170706f08c296068c6e84615cb6d6a659a9de
Monjur Elahi Touheed <monjurelahitouheed@gmail.com>
2025-05-31 12:53:59 +0600

    slider responsive complete

--- src/components/Content/Welcome.jsx
+++ src/components/Content/Welcome.jsx
@@ -6,20 +6,19 @@ import wel1 from '../../assets/welcome1.png'
 import wel2 from '../../assets/welcome2.png'
 import wel3 from '../../assets/welcome3.png'
 import wel4 from '../../assets/welcome4.png'
-// import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const Welcome = () => {
-    // const settings = {
-    //     dots: true,
-    //     infinite: true,
-    //     slidesToShow: 4,
-    //     slidesToScroll: 1,
-    //     autoplay: true,
-    //     speed: 2000,
-    //     autoplaySpeed: 2000,
-    //     cssEase: "linear"
-    // };
+    const settings = {
+        dots: false,
+        infinite: true,
+        slidesToShow: 4,
+        autoplay: true,
+        speed: 4000,
+        autoplaySpeed: 0,
+        cssEase: "linear"
+    };
     return (
         <div>
             <Container>
@@ -30,29 +29,29 @@ const Welcome = () => {
                     <Description className="px-5 md:px-0 md:w-[579px] text-center md:text-left">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution.</Description>
                 </Flex>
             </Container>
-            <Flex className="overflow-x-hidden justify-between">
-                <img src={wel1} alt="" />
-                <img src={wel2} alt="" />
-                <img src={wel3} alt="" />
-                <img src={wel4} alt="" />
-            </Flex>
-            {/* <div className="slider-container overflow-hidden">
+            {/* <Flex className="overflow-x-hidden gap- justify-between">
+                <img src={wel1} alt="#welcome" />
+                <img src={wel2} alt="#welcome" />
+                <img src={wel3} alt="#welcome" />
+                <img src={wel4} alt="#welcome" />
+            </Flex> */}
+            <div className="slider-container overflow-hidden">
 
-                <Slider  {...settings}>
+                <Slider {...settings}>
                     <div>
-                        <img src={wel1} alt="" />
+                        <img src={wel1} alt="#welcome" />
                     </div>
                     <div>
-                        <img src={wel2} alt="" />
+                        <img src={wel2} alt="#welcome" />
                     </div>
                     <div>
-                        <img src={wel3} alt="" />
+                        <img src={wel3} alt="#welcome" />
                     </div>
                     <div>
-                        <img src={wel4} alt="" />
+                        <img src={wel4} alt="#welcome" />
                     </div>
                 </Slider>
-            </div> */}
+            </div>
         </div>
     );
 };
